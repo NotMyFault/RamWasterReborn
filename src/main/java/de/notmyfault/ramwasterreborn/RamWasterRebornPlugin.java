3c7622c168c5dc47b87c2efa9f33d6697f8de0c1
NotMyFault <mc.cache@web.de>
2021-04-03 13:43:36 +0200

    Initial commit

---
+++ src/main/java/de/notmyfault/ramwasterreborn/RamWasterRebornPlugin.java
@@ -0,0 +1,20 @@
+package de.notmyfault.ramwasterreborn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Vector;
+
+public class RamWasterRebornPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Vector vector = new Vector<>();
+        while (true) {
+            byte[] bytes = new byte[1048576];
+            vector.add(bytes);
+            Runtime runtime = Runtime.getRuntime();
+            getLogger().info("Eating ram, nom nom nom; memory left: " + runtime.freeMemory());
+        }
+    }
+
+}
